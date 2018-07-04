e0c727f38e2695f6dcccfe27294c3c00491ac808
paulheisele@gmail.com <paulheisele@gmail.com>
2018-07-04 17:22:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,17 @@
 class Song
 
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, :genre
 
   @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
+    @genre = genre
     @all << self
   end
 
   def self.all
     @@all
-  end
-
-  def artist_name
-    if self.artist
-        self.artist.name
-    else
-      nil
-    end
   end
 end
