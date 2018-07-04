6af18194633d73fd4df5a39bd044beffb7203be3
paulheisele@gmail.com <paulheisele@gmail.com>
2018-07-04 17:21:49 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,31 @@
 class Genre
 
-  attr_accessor :genre 
+  attr_accessor :name 
+
+  @@all = []
 
   def initialize(name)
-    @genre = genre
+    @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def new_songs(name, artist)
+    Song.new(name, artist, self)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    songs.collect do |song|
+      song.artist
+    end
+  end
 end
